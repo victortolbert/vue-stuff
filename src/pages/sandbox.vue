edf98bfcf23151b7f0eb8ce51361375aed9e1623
Victor Tolbert <victor.tolbert@gmail.com>
2023-09-06 21:18:37 -0400

    wip

---
+++ src/pages/sandbox.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Sandbox</div>
+</template>
+
+<style scoped>
+label {
+  @apply text-xs block;
+}
+input {
+  @apply block text-sm w-full border rounded px-2 py-1;
+}
+select {
+  @apply block text-sm w-full border rounded px-2 py-1;
+}
+button {
+  @apply bg-primary-500 text-sm text-white rounded px-4 py-1 whitespace-nowrap;
+}
+</style>
